9fb31ac8386330f62e91d203e5f8c4c754e24a42
elyervesson <elyervesson@gmail.com>
2014-07-23 15:06:48 -0300

    APOSTILA DESENVOLVIMENTO WEB COM HTML, CSS E JAVASCRIPT

--- root/js/home.js
+++ root/js/home.js
@@ -1,11 +1,31 @@
-funcion validaBusca() {
+/*
+Mostra uma mensagem de erro
+function validaBusca() {
+  if (document.querySelector('#q').value == '') {
+      alert('Não podia ter deixado em branco a busca!');
+      return false;
+  }  
+}
+*/
+//Pinta o fundo do formulario de vermelho
+function validaBusca() {
   if(document.querySelector('#q').value == '') {
-	alert('Não podia ter deixado em branco a busca!');
-	return false; 
+    document.querySelector('#form-busca').style.background = 'red';
+	return false;
   }
 }
-document.querySelector('#form-busca').onsubmit = validaBusca;
 /* fazendo a associação da função com o evento
  *(função anterior deve ser executada quando o usuário disparar o 
  *evento de enviar o formulário (onsubmit). )
  */
+document.querySelector('#form-busca').onsubmit = validaBusca;
+
+var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+  bannerAtual = (bannerAtual +1) % 2;
+  document.querySelector('.destaque img').src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 4000);
